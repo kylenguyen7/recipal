026127dadc748e161bf716c2506fd9348f23d949
kylenguyen7 <44532855+kylenguyen7@users.noreply.github.com>
2022-02-18 16:35:29 -0800

    Woof!

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Meow</Text>
+      <Text>Woof!</Text>
       <StatusBar style="auto" />
     </View>
   );
