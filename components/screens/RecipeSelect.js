9b1b3db1fb1d12045b0261729fdd9551d7ef5bdc
kylenguyen7 <44532855+kylenguyen7@users.noreply.github.com>
2022-02-23 17:04:31 -0800

    Added first draft of Recipe Select page.

--- components/screens/RecipeSelect.js
+++ components/screens/RecipeSelect.js
@@ -1,32 +1,202 @@
-import { StyleSheet, Text, ScrollView, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, Image, Pressable, ImageBackground, View } from 'react-native';
+import { useState } from 'react';
+import { SearchableFlatList } from "react-native-searchable-list";
+import { SearchBar } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 import RecipalButton from '../RecipalButton'
 import Header from '../BackHeader'
+import Images from '../../constants/images';
+import Colors from '../../constants/colors'
 
 export default function RecipeSelect() {
-  let navigation = useNavigation();
+  const navigation = useNavigation();
+  const [searchTerm, setSearchTerm] = useState("");
 
-  function goToRecipeStep(recipe, stepNum) {
+  const data = [ 
+    {
+      id: 0,
+      title: "Fettucine Alfredo",
+      time: "1.5 hours",
+      image: Images.spoonNoBg
+    },
+    {
+      id: 1,
+      title: "Garlic Breadsticks",
+      time: "1.5 hours",
+      image: Images.spoonNoBg
+    },
+    {
+      id: 2,
+      title: "Caprese Pizza",
+      time: "1.5 hours",
+      image: Images.spoonNoBg
+    },
+    {
+      id: 3,
+      title: "Chicken Parm",
+      time: "1.5 hours",
+      image: Images.spoonNoBg
+    },
+    {
+      id: 4,
+      title: "Lasagna",
+      time: "1.5 hours",
+      image: Images.spoonNoBg
+    },
+  ]
+
+  const searchAttribute = "title";
+  const ignoreCase = true;
+
+  function renderRecipe(recipe) {
+    const { title, time, image } = recipe
+    return (
+      <Pressable style={styles.recipeContainer} onPress={() => {navigation.navigate("RecipeStep", {recipe: title, step: 1})}}>
+        <Image style={styles.recipeImg} source={image}/>
+        <View style={styles.recipeTextContainer}>
+          <Text style={styles.recipeTitle}>{title}</Text>
+          <Text style={styles.recipeSubtitle}>Time: {time}</Text>
+          <Text style={styles.recipeSubtitle}>Calories: {time}</Text>
+        </View>
+      </Pressable>
+    )
+  }
+
+  function goToIngredients(recipe, stepNum) {
     navigation.push("RecipeStep", {recipe: recipe, step: stepNum})
   }
 
-  return (
-    <SafeAreaView>
+  return ( 
+    <ImageBackground source={Images.butchers} style={styles.container}>
       <Header></Header>
-      <ScrollView>
-        <Text>Recipe select!</Text>
-        <RecipalButton text={'Fetuccine Alfredo'} width={300} height={50}
-                        onPress={() => goToRecipeStep('fetuccine-alfredo', 1)}></RecipalButton>
-      </ScrollView>
-    </SafeAreaView>
+      <View style={styles.content}>
+        <View style={styles.titleContainer}>
+          <View style={styles.titleTextContainer}>
+            <Text style={styles.titleText}>Italian</Text>
+            <Text style={styles.titleText}>Recipes</Text>
+          </View>
+          <Image source={Images.spoonInCircle} style={styles.titleImg}></Image>
+        </View>
+        <ImageBackground source={Images.notepad} style={styles.notebook}>
+          <View style={{width: '110%'}}>
+            <SearchBar
+              platform='ios'
+              placeholder="Search..."
+              onChangeText={(search) => setSearchTerm(search)}
+              value={searchTerm}
+            />
+          </View>  
+          <View style={styles.listContainer}>
+            <SearchableFlatList
+              contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}
+              style={styles.list} data={data} searchTerm={searchTerm}
+              searchAttribute={searchAttribute} ignoreCase={ignoreCase}
+              renderItem={({ item }) => renderRecipe(item)}
+              keyExtractor={(item) => item.id} />
+          </View>
+        </ImageBackground>
+      </View>
+    </ImageBackground>
   );
+
+  /**
+   * 
+   */
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white'
   },
+  content: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  titleContainer: {
+    height: 110,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleImg: {
+    height: '110%',
+    width: 110,
+    resizeMode: 'contain',
+    position: 'relative',
+    bottom: 10,
+  },
+  titleTextContainer: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 25
+  },
+  titleText: {
+    fontFamily: 'Avenir-Black',
+    color: 'black',
+    fontSize: 32,
+  },
+  notebook: {
+    padding: 30,
+    paddingLeft: 70,
+    resizeMode: 'contain',
+    height: 1300,
+    width: undefined,
+    aspectRatio: 384 / 1349,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  listContainer: {
+    height: 430,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  list: {
+    width: '100%'
+  },
+  recipeContainer: {
+    height: 100,
+    width: 250,
+    margin: 7,
+    borderRadius: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,  
+    elevation: 5
+  },
+  recipeImg: {
+    height: '80%',
+    width: '30%',
+    resizeMode: 'cover',
+    backgroundColor: Colors.tomato,
+  },
+  recipeTextContainer: {
+    marginLeft: 10,
+    width: 140
+  },
+  recipeTitle: {
+    fontFamily: 'Avenir-Book',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  recipeSubtitle: {
+    fontFamily: 'Avenir-Book',
+    color: 'black',
+    fontSize: 12
+  }
 });
