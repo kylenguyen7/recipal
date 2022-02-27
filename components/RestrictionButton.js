5eee6f6d217ae0056399d34217d7ca68a9890f7b
andyvhuynh01 <avhuynh@stanford.edu>
2022-02-27 01:16:35 -0800

    ds popups working, note: layout not done

--- components/RestrictionButton.js
+++ components/RestrictionButton.js
@@ -1,4 +1,6 @@
-import { StyleSheet, Text, Pressable, Image } from 'react-native';
+import { StyleSheet, Text, Pressable, Image, Modal, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useState, useRef } from 'react';
 import Colors from '../constants/colors';
 import Images from '../constants/images'
 
@@ -12,17 +14,57 @@ import Images from '../constants/images'
  * <RecipalButton width={200} height={50} text={'Press Me!'} onPress={onButtonPress}></RecipalButton>
  */
 
-export default function RestrictionButton({title, description, onPress }) {
+export default function RestrictionButton({title, description}) {
+  let navigation = useNavigation()
+  const [finishModalVisible, setFinishModalVisible] = useState(false);
+  const [finishModalText, setFinishModalText] = useState("")
+  const [modalTitleText, setModalTitleText] = useState("")
+  
+  const ConfirmFinishModal = () => (
+    <Modal
+        animationType="slide"
+        transparent={true}
+        visible={finishModalVisible}
+        onRequestClose={() => {
+          setFinishModalVisible(false);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+          <Text style={[styles.modalText, {fontSize: 16}]}>{modalTitleText}</Text>
+            <Text style={[styles.modalText, {fontSize: 16}]}>{finishModalText}</Text>
+            <View style={styles.modalButtonContainer}>
+              <Pressable
+                style={[styles.button, styles.buttonCancel]}
+                onPress={() => {
+                  setFinishModalVisible(false);
+                }}
+              >
+                <Text style={[styles.textStyle, styles.cancelTextStyle]}>All done!</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+  );
+
   return (
-    <Pressable onPress={onPress} style={styles.button}>
-        <Text style={styles.buttonText}>{title}</Text>
+    <Pressable style={styles.restrictButton}>
+      <ConfirmFinishModal/>
+      <Text style={styles.buttonText}>{title}</Text>
+      <Pressable onPress={() => {
+        setFinishModalText(description);
+        setModalTitleText(title);
+        setFinishModalVisible(true);
+      }}>
         <Image style={styles.infoButton} source={Images.infoButton}/>
+      </Pressable>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
+  restrictButton: {
     height: 70,
     width: '48%',
     flexDirection: 'row',
@@ -74,6 +116,71 @@ const styles = StyleSheet.create({
   infoButton: {
     height: 30,
     width: 30,
-  }
+  },
+
+
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalButtonContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  button: {
+    borderRadius: 1000,
+    padding: 10,
+    elevation: 2,
+    borderColor: Colors.tomato,
+    borderWidth: 3
+  },
+  buttonExit: {
+    backgroundColor: Colors.white,
+  },
+  buttonCancel: {
+    backgroundColor: Colors.tomato,
+  },
+  textStyle: {
+    fontFamily: 'Avenir-Book',
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  exitTextStyle: {
+    color: Colors.tomato
+  },
+  cancelTextStyle: {
+    color: 'white'
+  },
+  modalText: {
+    fontFamily: 'Avenir-Book',
+    fontSize: 14,
+    marginBottom: 15,
+    textAlign: "center"
+  },
 });
   
--- components/screens/DietRestrictScreen.js
+++ components/screens/DietRestrictScreen.js
@@ -1,12 +1,45 @@
-import { StyleSheet, Text, View, Image, FlatList,  } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, Modal, Pressable } from 'react-native';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import Header from '../BackHeader'
 import Images from '../../constants/images';
-import { restrictions } from './restrictionsData';
+import Colors from '../../constants/colors';
+import { restrictions } from '../restrictionsData';
 import RestrictionButton from '../RestrictionButton';
 
 
 export default function DietRestrictScreen() {
+  // let navigation = useNavigation()
+  // const [finishModalVisible, setFinishModalVisible] = useState(true);
+  //   const ConfirmFinishModal = () => (
+  //   <Modal
+  //       animationType="slide"
+  //       transparent={true}
+  //       visible={finishModalVisible}
+  //       onRequestClose={() => {
+  //         setFinishModalVisible(false);
+  //       }}
+  //     >
+  //       <View style={styles.centeredView}>
+  //         <View style={styles.modalView}>
+  //           <Text style={[styles.modalText, {fontSize: 16}]}>Congratulations on finishing this recipe! Are you sure you'd like to complete the recipe?</Text>
+  //           <View style={styles.modalButtonContainer}>
+  //             <Pressable
+  //               style={[styles.button, styles.buttonCancel]}
+  //               onPress={() => {
+  //                 setFinishModalVisible(false);
+  //               }}
+  //             >
+  //               <Text style={[styles.textStyle, styles.cancelTextStyle]}>All done!</Text>
+  //             </Pressable>
+  //           </View>
+  //         </View>
+  //       </View>
+  //     </Modal>
+  // );
+
+
   function renderRestriction(restriction) {
     return (
       <RestrictionButton title={restriction.title} description={restriction.description}/>
@@ -17,6 +50,7 @@ export default function DietRestrictScreen() {
   return (
     <View>
       <Header></Header>
+      {/* <ConfirmFinishModal/> */}
       <View style={styles.content}>
         
         <View style={styles.titleContainer}>
@@ -85,5 +119,124 @@ const styles = StyleSheet.create({
     width: '90%',
     margin: 15,
     marginBottom: 474
+  },
+
+
+
+
+  container: {
+    flex: 1,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalButtonContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  button: {
+    borderRadius: 1000,
+    padding: 10,
+    elevation: 2,
+    borderColor: Colors.tomato,
+    borderWidth: 3
+  },
+  buttonExit: {
+    backgroundColor: Colors.white,
+  },
+  buttonCancel: {
+    backgroundColor: Colors.tomato,
+  },
+  textStyle: {
+    fontFamily: 'Avenir-Book',
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  exitTextStyle: {
+    color: Colors.tomato
+  },
+  cancelTextStyle: {
+    color: 'white'
+  },
+  modalText: {
+    fontFamily: 'Avenir-Book',
+    fontSize: 14,
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  stepHeader: {
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  stepHeaderText: {
+    fontFamily: 'Avenir-Book',
+    fontSize: 32
+  },
+  card: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  cardImg: {
+    width: 300,
+    height: 250,
+    borderRadius: 10,
+    marginVertical: 20,
+    resizeMode: 'cover',
+  },
+  cardTextContainer: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 2,
+    borderColor: 'black',
+    borderWidth: 3,
+  },
+  controlsContainer: {
+    height: 100,
+    width: '100%',
+    paddingVertical: 10,
+    position: 'absolute',
+    bottom: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  controlPressable: {
+    borderRadius: 100,
+    height: '100%',
+    aspectRatio: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
