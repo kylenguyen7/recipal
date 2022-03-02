8615766a508f4a38a1c76931ff15ba1afe2f4b4e
andyvhuynh01 <avhuynh@stanford.edu>
2022-03-01 23:32:55 -0800

    added desc for nutition, note: which items to keep?

--- components/nutritionData.js
+++ components/nutritionData.js
@@ -4,119 +4,119 @@ export const nutritionData = [
     {
         id: 0,
         title: 'CALORIES', 
-        description:'[DESCRIPTION HERE]',
+        description:'Units commonly used to measure energy content of foods and beverages as well as energy use (expenditure) by the body. A kilocalorie is equal to the amount of energy (heat) required to raise the temperature of 1 kilogram of water 1 degree centigrade. Energy is required to sustain the body\'s various functions, including metabolic processes and physical activity. Carbohydrate, fat, protein, and alcohol provide all of the energy supplied by foods and beverages. If not specified explicitly, references to "calories" refer to "kilocalories."',
         units: 'cal',
         image: '[IMAGE HERE]'
     },
     {
         id: 1,
         title: 'TOTAL FAT', 
-        description:'[DESCRIPTION HERE]',
+        description:'Indicates how much fat is in a single serving of food or drink. Fat is one of three main nutrients in food that provide calories, or "energy," for the body. Each gram of fat provides 9 calories. Fat is necessary for proper growth and development, helps the body absorb fat-soluble vitamins, and supports key body processes (such as blood clotting, nervous system function, reproduction, and immune response).',
         units: 'g',
         image: '[IMAGE HERE]'
     },
     {
         id: 2,
         title: 'SATURATED FAT', 
-        description:'[DESCRIPTION HERE]',
+        description:'Fatty acids that have no double bonds. Fats high in saturated fatty acids are usually solid at room temperature. Sources include animal products (such as meat and dairy products) and tropical oils (such as coconut and palm oils).',
         units: 'g',
         image: '[IMAGE HERE]'
     },
     {
         id: 3,
         title: 'TRANS FAT', 
-        description:'[DESCRIPTION HERE]',
+        description:'[Unsaturated fatty acids that are structurally different from the unsaturated fatty acids that occur naturally in plant foods and that have detrimental health effects. Sources of trans fatty acids include partially hydrogenated vegetable oils, which were used in a variety of foods (such as baked goods, coffee creamer, ready-to use frostings, snack foods, and stick margarine). As of 2018, most uses of partially hydrogenated oils have been phased out. Trans fatty acids are also present naturally in foods that come from ruminant animals (such as dairy products, beef, and lamb) and at very low levels in refined vegetable oils.',
         units: 'g',
         image: '[IMAGE HERE]'
     },
     {
         id: 4,
         title: 'CHOLESTEROL', 
-        description:'[DESCRIPTION HERE]',
+        description:'A natural sterol present in all animal tissues. Free cholesterol is a component of cell membranes and serves as a precursor for steroid hormones (estrogen, testosterone, aldosterone), and for bile acids. Humans are able to synthesize sufficient cholesterol to meet biologic requirements, and there is no evidence for a dietary requirement for cholesterol.',
         units: 'mg',
         image: '[IMAGE HERE]'
     },
     {
         id: 5,
         title: 'SODIUM', 
-        description:'[DESCRIPTION HERE]',
+        description:'A mineral and one of the chemical elements found in salt. Sodium is an essential nutrient needed by the human body in relatively small amounts (provided that substantial sweating does not occur) and is important for many body processes, such as fluid balance, muscle contraction, and nervous system function.',
         units: 'mg',
         image: '[IMAGE HERE]'
     },
     {
         id: 6,
         title: 'TOTAL CARBOHYDRATES', 
-        description:'[DESCRIPTION HERE]',
+        description:'Indicates how many grams of carbohydrate are in a single serving of food or drink. Carbohydrates are one of three main nutrients in food that provide calories, or "energy" for the body. Each gram of carbohydrate provides 4 calories. The human body breaks down carbohydrates into glucose, which is the primary energy source for the body\'s cells, tissues, and organs.',
         units: 'g',
         image: '[IMAGE HERE]'
     },
     {
         id: 7,
         title: 'DIETARY FIBER', 
-        description:'[DESCRIPTION HERE]',
+        description:'A type of carbohydrate that cannot be easily digested in the small intestine. Dietary fiber can increase the frequency of bowel movements, lower blood glucose and cholesterol levels, and reduce calorie intake. Dietary Fiber on the Nutrition Facts label includes naturally occurring fibers in plants (such as beans, fruits, nuts, peas, vegetables, seeds, whole grains, and foods made with whole grain ingredients) and certain isolated or synthetic non-digestible carbohydrates added to food that the U.S. Food and Drug Administration has determined have beneficial physiological effects to human health. ',
         units: 'g',
         image: '[IMAGE HERE]'
     },
     {
         id: 8,
         title: 'SUGARS', 
-        description:'[DESCRIPTION HERE]',
+        description:'Sugars are the smallest and simplest type of carbohydrate and provide a sweet taste. The human body breaks down sugars into glucose, which is the main energy source for the body\'s cells, tissues, and organs.',
         units: 'g',
         image: '[IMAGE HERE]'
     },
     {
         id: 9,
         title: 'PROTEIN', 
-        description:'[DESCRIPTION HERE]',
+        description:'One of three main nutrients in food that provide calories, or "energy," for the body. Each gram of protein provides 4 calories. Proteins are made up of amino acids and are needed for the body to function properly. They are the basis of body structures, such as skin and hair, and are important for many body processes.',
         units: 'g',
         image: '[IMAGE HERE]'
     },
     {
         id: 10,
         title: 'CALCIUM', 
-        description:'[DESCRIPTION HERE]',
+        description:'A mineral important for optimal bone health. It is also important for many body processes, such as blood clotting, hormone secretion, muscle contraction, and nervous system function.',
         units: 'mg',
         image: '[IMAGE HERE]'
     },
     {
         id: 11,
         title: 'IRON', 
-        description:'[DESCRIPTION HERE]',
+        description:'A mineral important for red blood cell formation. It is also important for many body processes, such as growth and development, immune function, reproduction, and wound healing.',
         units: 'mg',
         image: '[IMAGE HERE]'
     },
-    {
-        id: 12,
-        title: 'POTASSIUM', 
-        description:'[DESCRIPTION HERE]',
-        units: 'mg',
-        image: '[IMAGE HERE]'
-    },
+    // {
+    //     id: 12,
+    //     title: 'POTASSIUM', 
+    //     description:'[DESCRIPTION HERE]',
+    //     units: 'mg',
+    //     image: '[IMAGE HERE]'
+    // },
     {
         id: 13,
         title: 'VITAMIN A', 
-        description:'[DESCRIPTION HERE]',
+        description:'Vitamin A is a fat-soluble vitamin that is naturally present in many foods. Vitamin A is important for normal vision, the immune system, and reproduction. Vitamin A also helps the heart, lungs, kidneys, and other organs work properly.',
         units: 'mg',
         image: '[IMAGE HERE]'
     },
-    {
-        id: 14,
-        title: 'VITAMIN B', 
-        description:'[DESCRIPTION HERE]',
-        units: 'mg',
-        image: '[IMAGE HERE]'
-    },
+    // {
+    //     id: 14,
+    //     title: 'VITAMIN B', 
+    //     description:'[DESCRIPTION HERE]',
+    //     units: 'mg',
+    //     image: '[IMAGE HERE]'
+    // },
     {
         id: 15,
         title: 'VITAMIN C', 
-        description:'[DESCRIPTION HERE]',
+        description:'Vitamin C, also known as ascorbic acid, is a water-soluble nutrient found in some foods. In the body, it acts as an antioxidant, helping to protect cells from the damage caused by free radicals. Free radicals are compounds formed when our bodies convert the food we eat into energy. The body also needs vitamin C to make collagen, a protein required to help wounds heal. In addition, vitamin C improves the absorption of iron from plant-based foods and helps the immune system work properly to protect the body from disease.',
         units: 'mg',
         image: '[IMAGE HERE]'
     },
     {
         id: 16,
         title: 'VITAMIN D', 
-        description:'[DESCRIPTION HERE]',
+        description:'A vitamin that helps your body absorb calcium and is important for optimal bone health. It is also important for many body processes, such as blood pressure regulation, hormone production, and immune and nervous system function. Vitamin D is found in many foods and is also produced by the body when your skin is exposed to sunlight.',
         units: 'mg',
         image: '[IMAGE HERE]'
     },
